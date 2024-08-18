aaaa2cdf36ee809ff0cc28381bd15e3a72419ca0
Anastasia Belosludceva <anastasia.tsyganova2017@yandex.ru>
2024-08-18 23:06:20 +0500

    Перенос страниц

---
+++ src/components/header/header.tsx
@@ -0,0 +1,36 @@
+
+import iglogo from './threads-logo.png';
+import styles from './header.module.css';
+import home from './home.png';
+import human from './human.png';
+import like from './like.png';
+import search from './search.png';
+import post from './post.png';
+import Image from 'next/image';
+
+ const Header = () => {
+    return (
+        <header>
+    <nav className={styles.container}>
+        <div className={styles.nav}>
+
+        <Image src={iglogo} alt="logo"></Image>
+
+            <ul className={styles.ul}>
+                <button className={styles.btn} ><Image src={home} alt="home"></Image></button>
+                <button className={styles.btn}><Image src={search} alt="home"></Image></button>
+                <button className={styles.btn}><Image src={post} alt="home"></Image></button>
+                <button className={styles.btn}><Image src={like} alt="home"></Image></button>
+                <button className={styles.btn}><Image src={human} alt="home"></Image></button>
+            </ul>
+
+            <div className={styles.login}>
+                Log in
+            </div>
+        </div>
+    </nav>
+    </header>
+    )
+}
+
+export default Header
