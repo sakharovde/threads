7b41725b4ee3d04c4b0554bcf58697e381624f32
Ulyana Sakharova <u.a.saharova@mail.ru>
2024-09-14 18:27:18 +0300

    Добавили имя и аватар

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,30 @@
 import { Header } from '@/components';
+import styles from './styles.module.css';
+import { faker } from '@faker-js/faker';
+import Image from 'next/image';
 
 export default function Profile() {
   return (
     <div>
       <Header />
-      <main>lolkek </main>
+      <main>
+        <div className={styles.header}>
+          <div>
+            <div className={styles.name}>{faker.person.fullName()}</div>
+            <div>{faker.internet.userName()}</div>
+          </div>
+          <div>
+            <div className={styles.avatar}>
+              <Image
+                src={faker.image.avatar()}
+                height={84}
+                width={84}
+                alt='avatar'
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
