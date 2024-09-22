f5c7970575a1b2eefc1091c6ddb93aed4cf32daa
Anastasia Belosludceva <anastasia.tsyganova2017@yandex.ru>
2024-09-22 23:45:25 +0500

    atomic styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,29 +11,28 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header>
-      <nav className={styles.container}>
-        <div className={styles.nav}>
-          <Image src={iglogo} alt='logo'></Image>
-
-          <ul className={styles.ul}>
-            <button className={styles.btn}>
-              <Image src={home} alt='home'></Image>
+      <nav className='w-screen min-h-12 mb-8'>
+        <div className='w-full flex items-center content-between inset-x-0 bottom-0 mx-0 my-autos'>
+          <Image src={iglogo} width={60} height={60} alt='logo'></Image>
+          <ul className='flex-1 list-none text-center cursor-pointer'>
+            <button className='inline-block rounded-lg hover:bg-zinc-100 py-4 px-8'>
+              <Image src={home} width={25} height={25} alt='home'></Image>
             </button>
-            <button className={styles.btn}>
-              <Image src={search} alt='home'></Image>
+            <button className='inline-block rounded-lg hover:bg-zinc-100 py-4 px-8'>
+              <Image src={search} width={25} height={25} alt='home'></Image>
             </button>
-            <button className={styles.btn}>
-              <Image src={post} alt='home'></Image>
+            <button className='inline-block rounded-lg hover:bg-zinc-100 py-4 px-8'>
+              <Image src={post} width={25} height={25} alt='home'></Image>
             </button>
-            <button className={styles.btn}>
-              <Image src={like} alt='home'></Image>
+            <button className='inline-block rounded-lg hover:bg-zinc-100 py-4 px-8'>
+              <Image src={like} width={25} height={25} alt='home'></Image>
             </button>
-            <button className={styles.btn}>
-              <Image src={human} alt='home'></Image>
+            <button className='inline-block rounded-lg hover:bg-zinc-100 py-4 px-8'>
+              <Image src={human} width={25} height={25} alt='home'></Image>
             </button>
           </ul>
 
-          <div className={styles.login}>
+          <div className='rounded-xl bg-black text-white py-1 px-5 cursor-pointer'>
             <Link href='/login'> Log in </Link>
           </div>
         </div>
